b10fd8e5f25d7c2b51b95a41872623eb3b930d72
Rushil Patel <rushildineshpatel2000@gmail.com>
2024-05-17 03:16:49 -0400

    added components - Button, ButtonWarning, Heading, InputBox, SubHeading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Signup from "./pages/signup";
-import Signin from "./pages/signin";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
 import Dashboard from "./pages/Dashboard";
 import SendMoney from "./pages/SendMoney";
 
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,5 +1,15 @@
-function Signup() {
-  return <div>signup page</div>;
+import Heading from "../components/Heading";
+import SubHeading from "../components/SubHeading";
+import InputBox from "../components/InputBox";
+import Button from "../components/Button";
+import ButtonWarning from "../components/ButtonWarning";
+
+function onClick() {
+  return null;
 }
+
+const Signup = () => {
+  return <></>;
+};
 
 export default Signup;
